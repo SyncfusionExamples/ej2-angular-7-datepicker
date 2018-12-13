10c12e4e1aee3bd087ccfaf87d17b574e51f1d0f
NAVIN V <navin.vinayagam@syncfusion.com>
2018-12-13 19:39:48 +0530

    Samples Added

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { DatePickerModule } from '@syncfusion/ej2-angular-calendars';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, DatePickerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
